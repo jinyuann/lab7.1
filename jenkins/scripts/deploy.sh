23b6115e8b713a00049e0d51c941414441a478c0
Jin Yuan <Jyneo96@gmail.com>
2021-10-28 14:18:46 +0800

    a
    a

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:81 --name my-apache-php-app -v c:\\users\\JIN YUAN NEO\\Documents\\Github\\lab7.1\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v c:\\users\\JIN YUAN NEO\\Documents\\Github\\lab7.1\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.10.109 to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
 
