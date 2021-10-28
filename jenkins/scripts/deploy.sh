283b04230e953738358ec0f16f38fa2a15b6c543
Jin Yuan <Jyneo96@gmail.com>
2021-10-28 13:59:52 +0800

    changed port
    changed port

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\users\\JIN YUAN NEO\\Documents\\Github\\lab7.1\\src:/var/www/html php:7.2-apache
+docker run -d -p 50:50 --name my-apache-php-app -v c:\\users\\JIN YUAN NEO\\Documents\\Github\\lab7.1\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost:5050 to see your PHP application in action.'
 
