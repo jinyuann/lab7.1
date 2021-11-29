ee79ce90519cc81cdde14327edd8041cfc67bc3e
someone <someone@someplace.com>
2021-11-29 17:25:12 +0000

    a

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://192.168.10.109";
+	String url = "http://139.59.227.176/";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
