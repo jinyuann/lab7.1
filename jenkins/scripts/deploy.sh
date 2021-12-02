e1658b2600b27306e4d4155c38ff501184425c19
jinyuan <jyneo96@gmail.com>
2021-12-02 05:05:04 +0000

    asd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 81:81 --name my-apache-php-app -v "/root/github/lab7.1"\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "/home/student145/test/lab7.1"/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://13.58.241.171 to see your PHP application in action.'
 
--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://139.59.227.176/";
+	String url = "http://13.58.241.171/";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
